395441e19f06f65d5e55b4b8e27c44cc3c4b64f6
unknown <marioposadamiranda@gmail.com>
2022-06-17 20:32:55 -0300

    setState

--- src/components/Estado.js
+++ src/components/Estado.js
@@ -1,17 +1,33 @@
 import React ,{Component} from "react";
 
+
+
+function EstadoAHijo(props){
+    return(
+        <div>
+<h3>{props.contadorHijo}</h3>
+        </div>
+    )
+}
+
 export default class Estado extends Component {
     constructor(props){
         super(props);
         this.state = {
             contador: 0,
         }
-    }
+        setInterval(() => {
+            this.setState({
+                contador: this.state.contador + 1,
+            })
+        }, 1000);
+    };
 
 render(){
 return(
     <div>
-        <h2>El State</h2>
+        <h2>{this.state.contador}</h2>
+        <EstadoAHijo contadorHijo={this.state.contador}/>
     </div>
 )
 
