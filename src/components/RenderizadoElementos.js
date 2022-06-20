79c09e5cd9f73feb8fef2137f14d0db0dfa0883c
unknown <marioposadamiranda@gmail.com>
2022-06-19 23:27:23 -0300

    List

--- src/components/RenderizadoElementos.js
+++ src/components/RenderizadoElementos.js
@@ -1,11 +1,25 @@
 import React, {Component} from "react";
 
 export default class RenderizadoElementos extends Component {
+constructor(props){
+    super(props);
+this.state = {
+    estaciones: ["Primaver","verano","otoño","invierno"]
+}
+
+}
+
     render()
     {
 return(
     <div>
         <h2>REnderizado de Elementos</h2>
+        <h3>Estaciones del año</h3>
+        <ol>
+            {
+                this.state.estaciones.map((el,index)=> <li key={index}>{el}</li>)
+            }
+        </ol>
     </div>
 )
     }
