c66a48a0a803cf1a966e889bb136b4e518268aaf
unknown <marioposadamiranda@gmail.com>
2022-06-20 00:02:33 -0300

    Props ul

--- src/components/RenderizadoElementos.js
+++ src/components/RenderizadoElementos.js
@@ -1,4 +1,15 @@
 import React, {Component} from "react";
+import data from "../helpers/data.json";
+
+
+function ElementosLista(props){
+        return(
+            <li>
+                <a href={props.el.web}>{props.el.name}</a>
+            </li>
+    )
+    
+}
 
 export default class RenderizadoElementos extends Component {
 constructor(props){
@@ -11,6 +22,8 @@ this.state = {
 
     render()
     {
+        console.log(data);
+        
 return(
     <div>
         <h2>REnderizado de Elementos</h2>
@@ -20,6 +33,10 @@ return(
                 this.state.estaciones.map((el,index)=> <li key={index}>{el}</li>)
             }
         </ol>
+        <h3>Frameworks FrontEnd Javascript</h3>
+        <ul>
+            {data.frameworks.map((el,index)=> <ElementosLista key={index} el={el}/>)}
+        </ul>
     </div>
 )
     }
