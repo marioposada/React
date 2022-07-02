273fb35b443f930389dd1ef767bf8d29f6f5c2b7
unknown <marioposadamiranda@gmail.com>
2022-07-02 19:33:41 -0300

    toLOcale

--- src/components/CicloVida.js
+++ src/components/CicloVida.js
@@ -3,17 +3,22 @@ import React, {Component} from "react";
 export default class CicloVida extends Component {
     constructor(props) {
 super(props);
+console.log(0, "El componente se inicializa pero no esta en el DOM")
 
-console.log(0, "El componente se inicializa")
+this.state = {
+    hora: new Date().toLocaleTimeString()
+}
     }
 
 
     render(){
+    console.log(4, "El componente se dibuja por algun cambio en el DOM")
         return(
 
 
             <>
             <h2>Ciclo de vide de los componentes</h2>
+            <h3>{this.state.hora}</h3>
             </>
         )
     }
