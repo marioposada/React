e5e3cde1b4ab4cea6443486cdd855813ff67d139
unknown <marioposadamiranda@gmail.com>
2022-07-03 15:11:46 -0300

    ComponentDid

--- src/components/CicloVida.js
+++ src/components/CicloVida.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 
+
 export default class CicloVida extends Component {
+   
+   
     constructor(props) {
 super(props);
 console.log(0, "El componente se inicializa pero no esta en el DOM")
@@ -9,14 +12,42 @@ this.state = {
     hora: new Date().toLocaleTimeString()
 }
 
-iniciar = () => {
+this.temporizador = null;
 
 }
+/********* */
 
-detener = () => {
+componentDidMount() {
+    console.log(1, "El componente ya se encuentra en el DOM")
+}
+
+componentDidUpdate(prevProps,prevState){
+    console.log(2, "El estado o las props del componente han cambiado");
+    console.log(prevProps)
+    console.log(prevState)
     
 }
-    }
+
+tictac = () => {
+    this.temporizador = setInterval(() => {
+        this.setState({
+            hora: new Date().toLocaleTimeString()
+        });
+        
+    }, 1000);
+}
+/******** */
+
+iniciar = () => {
+    this.tictac();
+
+};
+
+detener = () => {
+    clearInterval(this.temporizador)
+    
+};
+    
 
 
     render(){
