f14d6a234d3dd51dec7ba552d6d7e2aecd6e11c6
unknown <marioposadamiranda@gmail.com>
2022-07-03 13:56:45 -0300

    Components

--- src/components/CicloVida.js
+++ src/components/CicloVida.js
@@ -8,6 +8,14 @@ console.log(0, "El componente se inicializa pero no esta en el DOM")
 this.state = {
     hora: new Date().toLocaleTimeString()
 }
+
+iniciar = () => {
+
+}
+
+detener = () => {
+    
+}
     }
 
 
@@ -19,6 +27,8 @@ this.state = {
             <>
             <h2>Ciclo de vide de los componentes</h2>
             <h3>{this.state.hora}</h3>
+            <button onClick={this.iniciar}>Iniciar</button>
+            <button onClick={this.detener}>Detener</button>
             </>
         )
     }
