974888190a7d3bc21c8105754ee943fd7b314457
unknown <marioposadamiranda@gmail.com>
2022-07-04 20:41:34 -0300

    Fetch

--- src/components/AjaxApis.js
+++ src/components/AjaxApis.js
@@ -1,6 +1,45 @@
 import React, { Component } from 'react';
 
+function Pokemon(props){
+    return(
+        <figure>
+            <img src={props.avatar} alt={props.name} />
+            <figcaption>{props.name}</figcaption>
+            
+        </figure>
+    )
+}
+
 export default class AjaxApis extends Component {
+    state = {
+        pokemons: []
+    }
+
+    componentDidMount(){
+       let url="https://pokeapi.co/api/v2/pokemon/"
+       fetch(url)
+       .then(res => res.json())
+       .then(json => {
+        console.log(json);
+        json.results.forEach(element => {
+            fetch(element.url)
+            .then(res => res.json())
+            .then(json =>{
+                let pokemon = {
+                    id: json.id,
+                    name: json.name,
+                    avatar: json.sprites.front_default
+                }
+            })
+                
+        });
+
+
+
+
+
+       })
+    }
     render(){
         return(
             <>
