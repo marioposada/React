343a815a1eea6f2da5f20b4b0a1ae797ca34f1eb
unknown <marioposadamiranda@gmail.com>
2022-07-12 00:51:05 -0300

    v01

--- src/components/HooksPersonalizados.js
+++ src/components/HooksPersonalizados.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { useFetch } from "../hooks/useFetch";
 
 export default function HooksPersonalizados() {
 
+   let url = "https://jsonplaceholder.typicode.com/photos"
 
 
+let {data,isPending,error} = useFetch(url);
+console.log(JSON.stringify(data))
 
 
-
-
-
-
-    
   return (
     <>
       <h2> Hooks Personalizados</h2>
-    </>
-  );
+      <h3>{JSON.stringify(isPending)}</h3>
+      <h3 style={{whiteSpace:"pre-wrap"}}>{JSON.stringify(data)}</h3>
+      <h3>{JSON.stringify(error)}</h3>
+    
+</>
+  )
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,29 +1,36 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 export const useFetch = (url) => {
-const [data, setData] = useState(null);
-const [isPending, setIsPending] = useState(true);
-const [error, setError] = useState(null)
-
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    const getData = async (url) => {
+      try {
+        let res = await fetch(url);
 
-    const getData = (url) => {
-try {
-    
-} catch (error) {
-    
-}
+        if (!res.ok) {
+         throw new Error ({
+            err: true,
+            status: res.status,
+            statusText: !res.statusText ? "Ocurrió un error" : res.statusText,
+          })
+        }
 
+        let data = await res.json();
 
-    }
-    
-
-    return () => {
-      
+        setIsPending(false);
+        setData(data);
+        setError({ err: false });
+      } catch (err) {
+        setIsPending(true);
+        setError(err);
+      }
     };
+
+    getData(url);
   }, [url]);
 
-  return [data,isPending,error]
+  return { data, isPending, error };
 };
-
