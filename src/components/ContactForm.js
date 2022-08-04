cedaf370f4c12ddd204c64fa1027d4927012e7e7
unknown <marioposadamiranda@gmail.com>
2022-08-04 20:18:34 -0300

    validate

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "../hooks/useForm";
+
+const initialForm = {};
+const validationsForm = (form) => {};
 
 const ContactForm = () => {
-  return (
-    <div>ContactForm</div>
-  )
-}
+  const {
+    form,
+    error,
+    loading,
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = useForm(initialForm, validationsForm);
+  return <div>ContactForm</div>;
+};
 
-export default ContactForm
+export default ContactForm;
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,16 +1,28 @@
 import { useState } from "react";
 
-export const useForm = (initialForm) => {
+export const useForm = (initialForm,validateForm) => {
   const [form, setForm] = useState(initialForm);
 
   const [error, setError] = useState({});
   const [loading, setLoading] = useState(false);
   const [response, setResponse] = useState(null);
 
+  // Registro de los imputs
+  const handleChange = () => {};
 
-const handleChange = () => {};
-const handleBlur = () => {};
-const handleSubmit = () => {};
+  // Aqui se analizan las verificaciones, cuando los elementos pierden el foco
+  const handleBlur = () => {};
 
+  // Envio del formulario
+  const handleSubmit = () => {};
 
+  return {
+    form,
+    error,
+    loading,
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  };
 };
