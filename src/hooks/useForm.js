1a349757b10bc4c0ec9a602da6f96882e05efb8a
unknown <marioposadamiranda@gmail.com>
2022-08-04 20:06:58 -0300

    Form

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import { useState } from "react";
 
-export const useForm = () => {
-  return (
-    <div>useForm</div>
-  )
-}
+export const useForm = (initialForm) => {
+  const [form, setForm] = useState(initialForm);
+
+  const [error, setError] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState(null);
+
+
+const handleChange = () => {};
+const handleBlur = () => {};
+const handleSubmit = () => {};
+
+
+};
